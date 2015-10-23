36562f7c673667d19a73d7cc901447647737fc59
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2015-10-23 11:24:36 +0200

    Ændret metoderne til at starte med små bogstaver

--- 38_Del2/src/spil/PlayerAccount.java
+++ 38_Del2/src/spil/PlayerAccount.java
@@ -4,12 +4,12 @@ public class PlayerAccount {
 	
 	private int balance;
 	
-	public void SetBalance(int balance) 
+	public void setBalance(int balance) 
 	{
 		this.balance = balance;
 	}
 	
-	public int GetBalance()
+	public int getBalance()
 	{
 		return balance;
 	}
