7fd6e8fa5c700bb5c12aa0f0463842c01f6e7ba9
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2015-10-28 09:59:52 +0100

    Slettet konto, og lagt dens metoder over i PlayerAccount

--- 38_Del2/src/spil/PlayerAccount.java
+++ 38_Del2/src/spil/PlayerAccount.java
@@ -4,14 +4,23 @@ public class PlayerAccount {
 	
 	private int balance;
 	
-	public void setBalance(int balance) 
-	{
-		this.balance = balance;
+	public void addSaldo(int amount) {
+		this.balance = this.balance + amount;
 	}
 	
-	public int getBalance()
-	{
-		return balance;
+	public void subSaldo(int amount) {
+		if((this.balance - amount)<0)
+			this.balance = 0;
+		else
+			this.balance = this.balance - amount;
+	}
+	
+	public void setSaldo(int amount) {
+		this.balance = amount;
+	}
+	
+	public int getSaldo() {
+		return this.balance;
 	}
 
 }
