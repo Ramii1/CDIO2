3d9eaec2bd5ec7ed8ed767924ef31f6ce92b419e
Silas El-Azm Stryhn <s143599@student.dtu.dk>
2015-11-03 14:24:41 +0100

    Har lavet en test der tester om playerAccount kan gå i minus.

---
+++ 38_Del2/src/test/TestGame.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import spil.Player;
+
+public class TestGame {
+
+	Player p;
+			
+	@Before
+	public void setUp() throws Exception {
+		p = new Player();
+		p.getPlayerAccount().setBalance(0);		assertEquals(0, p.getPlayerAccount().getBalance());
+	
+	}
+		
+	
+	@Test
+	public void test1() {
+		p.getPlayerAccount().setBalance(-1000);
+	
+	}
+	
+	
+	@Test
+	public void test2() {
+		p.getPlayerAccount().subBalance(500);
+	}
+	
+	
+	
+	@After
+	public void tearDown() throws Exception {
+		assertEquals(0, p.getPlayerAccount().getBalance());
+	}
+
+	
+
+}
